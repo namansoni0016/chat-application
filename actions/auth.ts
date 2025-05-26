1495144f30dc1ea2f02b4e913682221a83d2fb38
Naman Soni <namantsoni@gmail.com>
2025-05-26 22:54:17 +0530

    Middleware

--- actions/auth.ts
+++ actions/auth.ts
@@ -6,11 +6,11 @@ import { redirect } from "next/navigation";
 
 export async function getUserSession() {
     const supabase = await createClient();
-    const { data, error } = await supabase.auth.getSession();
+    const { data, error } = await supabase.auth.getUser();
     if(error) {
         return null;
     } 
-    return { status: "success", user: data.session?.user };
+    return { status: "success", user: data?.user };
 }
 
 export async function signUp(formData: FormData) {
--- app/chats/layout.tsx
+++ app/chats/layout.tsx
@@ -1,4 +1,3 @@
-// app/chats/layout.tsx
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import type { ReactNode } from "react";

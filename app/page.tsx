6969c0623e38c2435b521fadc2f062cd4d8de13a
Naman Soni <namantsoni@gmail.com>
2025-05-28 16:26:25 +0530

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const supabase = await createClient();
+  const { data: { user }} = await supabase.auth.getUser();
+  if(user) {
+    redirect("/chats");
+  }
   return (
-    <h1 className="text-2xl font-bold">Chat Application</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-green-700">
+      <h1 className="text-5xl font-bold text-white">Welcome to the <span className="text-black">Chat Application</span></h1>
+      <Button className="mt-4 font-bold shadow-2xl" variant="secondary">
+        <Link href="/signup">Get started...</Link>
+      </Button>
+    </div>
   );
 }
--- components/ChatSidebar.tsx
+++ components/ChatSidebar.tsx
@@ -8,6 +8,7 @@ import { Badge } from "./ui/badge";
 import { useEffect, useState } from "react";
 import { useChatStore } from "@/store/chatStore";
 import { IoSearch } from "react-icons/io5";
+import { IoFilterSharp } from "react-icons/io5";
 
 const ChatSidebar = () => {
     const [ users, setUsers ] = useState<{id: string, name: string, phone: string}[]>([]);
@@ -21,14 +22,17 @@ const ChatSidebar = () => {
         fetchUsers();
     }, []);
     return (
-        <aside className="h-full w-20 lg:w-90 border-r border-y border-base-300 flex flex-col transition-all duration-200">
+        <aside className="h-full w-20 lg:w-100 border-r border-y border-base-300 flex flex-col transition-all duration-200">
             <div className="border-b border-base-300 bg-muted/40 w-full p-3">
                 <div className="flex items-center gap-2">
                     <RiFolderDownloadFill className="text-green-600" />
                     <span className="text-sm font-bold text-green-600">Custom Filter</span>
-                    <Button variant="outline" className="rounded-sm text-[12px] p-1 h-7">Save</Button>
-                    <Button variant="outline" className="rounded-sm text-[12px] ml-10 w-20 h-7">
+                    <Button variant="outline" className="rounded-sm text-[12px] p-1 h-7 font-semibold">Save</Button>
+                    <Button variant="outline" className="rounded-sm text-[12px] ml-8 w-20 h-7 font-semibold">
                         <IoSearch className="text-[8px]" />Search
+                    </Button>
+                    <Button variant="outline" className="rounded-sm text-[12px] w-20 h-7">
+                        <IoFilterSharp className="text-[8px] text-green-600" /><span className="text-green-600 font-bold">Filtered</span>
                     </Button>
                 </div>
             </div>
@@ -40,7 +44,10 @@ const ChatSidebar = () => {
                         </div>
                         {/* User info */}
                         <div className="hidden lg:flex flex-col  w-full">
-                            <div className="text-sm font-bold text-start truncate">{user.name}</div>
+                            <div className="flex justify-between">
+                                <div className="text-sm font-bold text-start truncate">{user.name}</div>
+                                <Badge className="p-1 mr-2 text-xs bg-orange-50 text-orange-700">Demo</Badge>
+                            </div>
                             <p className="text-sm text-start text-gray-500 mb-1">Lorem ipsum dolor sit amet consec...</p>
                             <div className="flex justify-between items-center w-full">
                                 <Badge variant="secondary" className="flex items-center gap-0.5 text-gray-400 px-1 py-0.5 text-[10px] h-4" >
